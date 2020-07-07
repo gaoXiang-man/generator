1b0c0484093632b5b03e2aaa279ca4bf24d6e958
gao.xiang <799983831>
2020-07-07 14:53:44 +0800

    基础

---
+++ src/main/java/com/healthlink/doctor/dao/SQLServerGeneratorDao.java
@@ -0,0 +1,13 @@
+
+package com.healthlink.doctor.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * SQLServer代码生成器
+ *
+ */
+@Mapper
+public interface SQLServerGeneratorDao extends GeneratorDao {
+
+}
